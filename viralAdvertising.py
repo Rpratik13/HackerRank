aa153a1798dbf1978981ca0571e423753ffc9fe2
Pratik Rajbhandari <31899424+Rpratik13@users.noreply.github.com>
2020-01-12 22:16:04 +0545

    Add files via upload

--- viralAdvertising.py
+++ viralAdvertising.py
@@ -1,15 +1,15 @@
 def viralAdvertising(n):
 	cumulative = 2
 	liked = 2
-	for i in range(2,n+1):
+	for i in range(2, n + 1):
 		liked *= 3
 
-		if liked%2==0:
-			cumulative += liked//2
-			liked = liked//2
+		if liked % 2 == 0:
+			cumulative += liked // 2
+			liked //= 2
 		else:
-			cumulative +=(liked-1)//2
-			liked = (liked-1)//2
+			cumulative += (liked - 1) // 2
+			liked = (liked - 1) // 2
 	return cumulative
 
 n = int(input())
