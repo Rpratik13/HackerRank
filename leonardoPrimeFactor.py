5e8e48cc284a1a8b083d152b5c5a98af4ef9cf76
Pratik Rajbhandari <31899424+Rpratik13@users.noreply.github.com>
2020-01-17 21:02:51 +0545

    Add files via upload

--- leonardoPrimeFactor.py
+++ leonardoPrimeFactor.py
@@ -1,32 +1,32 @@
-from math import ceil, sqrt;
+from math import ceil, sqrt
 
 def isPrime(n):
-	val = ceil(sqrt(n));
-	if n==2:
-		return True;
-	if n%2==0 or n==1:
-		return False;
-	i=3;
-	while(i<val):
-		if n%i==0:
+	val = ceil(sqrt(n))
+	if n == 2:
+		return True
+	if n % 2 == 0 or n == 1:
+		return False
+	i = 3
+	while(i < val):
+		if n % i == 0:
 			return False;
-		i+=2;
-	return True;
+		i += ;
+	return True
 
 def primeFactors(inp):
-	ans = [];
+	ans = []
 	for i in inp:
-		count_prime=0;
-		for j in range(2,i+1):
-			if isPrime(j) and i%j==0:
-				count_prime+=1;
-		ans.append(count_prime);
-	return ans; 
+		count_prime=0
+		for j in range(2, i + 1):
+			if isPrime(j) and i % j == 0:
+				count_prime += 1
+		ans.append(count_prime)
+	return ans
 
 
 count = int(input());
-inp = [int(x) for x in input().split()];
+inp = [int(x) for x in input().split()]
 
-result = primeFactors(inp);
+result = primeFactors(inp)
 
-print(result);
+print(result)
